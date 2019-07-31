b423c3cf896b821e633d1d5de79d2999f8aea371
nghiem.tien.tuan <nghiem.tien.tuan@sun-asterisk.com>
2019-07-31 13:44:59 +0700

    login page

---
+++ routes/backend.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('admin/login', 'Auth\LoginController@getLoginAdmin')->name('admin.getLogin');
+Route::post('admin/login', 'Auth\LoginController@login')->name('admin.postLogin');
+
+Route::get('admin/first-login', 'FirstLoginController@getFirstLoginAdmin')->name('admin.getFirstLogin');
+Route::post('admin/first-login', 'FirstLoginController@postFirstLogin')->name('admin.postFirstLogin');
+
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
